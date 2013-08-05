cb41a187ad1f16a0ace44dff34fd50e40abefc7d
metal3d <metal3d@gmail.com>
2013-08-06 01:12:36 +0200

    The first and initial version

---
+++ verbalexpressions.go
@@ -0,0 +1,207 @@
+package verbalexpressions
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+	"log"
+)
+
+// VerbalExpression structure to create expression
+type VerbalExpression struct {
+	re         *regexp.Regexp
+	expression string
+	anycase    bool
+	oneline    bool
+}
+
+// quote is an alias to regexp.QuoteMeta
+func quote(s string) string {
+	return regexp.QuoteMeta(s)
+}
+
+// utility function to return only strings
+func tostring(i interface{}) string {
+	var r string
+	switch x := i.(type) {
+	case string:
+		r = x
+	case int64:
+		r =  strconv.FormatInt(x, 64)
+	case uint:
+		r = strconv.FormatUint(uint64(x), 64)
+	case int:
+		r = strconv.FormatInt(int64(x), 32)
+	default:
+		log.Panicf("Could not convert %v %t", x, x)
+	}
+	return r
+}
+
+// Instanciate a new VerbalExpression. You should use this method to
+// initalize some internal var.
+// Example:
+//		v := verbalexpression.New().Find("foo")
+func New() *VerbalExpression {
+	r := new(VerbalExpression)
+	r.anycase, r.oneline = false, false
+	r.re = &regexp.Regexp{}
+	return r
+}
+
+// add method, append expresions to the internal string that will be parsed
+func (v *VerbalExpression) add(s string) *VerbalExpression {
+	v.expression += s
+	return v
+}
+
+// Anything will match any char
+func (v *VerbalExpression) Anything() *VerbalExpression {
+	return v.add(`(.*)`)
+}
+
+// AnythingBut will match anything excpeting the given string.
+// Example:
+//		s := "This is a simple test"
+//		v := verbalexpressions.New().AnythingBut("ie").RegExp().FindAllString(s, -1)
+//		[Th s  s a s mple t st]
+func (v *VerbalExpression) AnythingBut(s string) *VerbalExpression {
+	return v.add(`([^` + quote(s) + `]*)`)
+}
+
+// EndOfLine tells verbalexpressions to match a end of line.
+// Warning, to check multiple line, you must use SearchOneLine(true)
+func (v *VerbalExpression) EndOfLine() *VerbalExpression {
+	return v.add(`$`)
+}
+
+// Maybe will search string zero on more times
+func (v *VerbalExpression) Maybe(s string) *VerbalExpression {
+	return v.add(`(` + quote(s) + `)?`)
+}
+
+// StartOfLine seeks the begining of a line. As EndOfLine you should use
+// SearchOneLine(true) to test multiple lines
+func (v *VerbalExpression) StartOfLine() *VerbalExpression {
+	return v.add(`^`)
+}
+
+// Find seeks string. The string MUST be there (unlike Maybe() method)
+func (v *VerbalExpression) Find(s string) *VerbalExpression {
+	return v.add(`(` + quote(s) + `)`)
+}
+
+// Alias to Find()
+func (v *VerbalExpression) Then(s string) *VerbalExpression {
+	return v.Find(s)
+}
+
+// Any accepts caracters to be matched
+// Example:
+//		s := "foo1 foo5 foobar"
+//		v := New().Find("foo").Any("1234567890").v.Regex().FindAllString(s, -1)
+//		[foo1 foo5]
+func (v *VerbalExpression) Any(s string) *VerbalExpression {
+	return v.add(`([` + quote(s) + `])`)
+}
+
+//AnyOf is an alias to Any
+func (v *VerbalExpression) AnyOf(s string) *VerbalExpression {
+	return v.Any(s)
+}
+
+// LineBreak to find "\n" or "\r\n"
+func (v *VerbalExpression) LineBreak() *VerbalExpression {
+	return v.add(`(\n|(\r\n))`)
+}
+
+// Alias to LineBreak
+func (v *VerbalExpression) BR() *VerbalExpression {
+	return v.LineBreak()
+}
+
+
+// Range accepts an even number of arguments. Each pair of values defines start and end of range.
+// Example:
+//		s := "This 1 is 55 a TEST"
+//		v := verbalexpressions.New().Range("a","z",0,9)
+func (v *VerbalExpression) Range(args ...interface{}) *VerbalExpression {
+	parts := make([]string, 3)
+	app := ""
+	for i := 0; i < len(args); i++ {
+		app += tostring(args[i])
+		if i%2 != 0 {
+			parts = append(parts, app)
+			app = ""
+		} else {
+			app += "-"
+		}
+	}
+	if len(args)%2 != 0 {
+		parts = append(parts, tostring(args[len(args)-1]))
+	}
+
+	return v.add("[" + strings.Join(parts, "") + "]")
+}
+
+// Tab fetch tabulation char (\t)
+func (v *VerbalExpression) Tab() *VerbalExpression {
+	return v.add(`\t`)
+}
+
+// Word matches any word (containing alpha char)
+func (v *VerbalExpression) Word() *VerbalExpression {
+	return v.add(`(\w+)`)
+}
+
+// Or, as the word is meaning...
+func (v *VerbalExpression) Or() *VerbalExpression {
+	return v.add("|")
+}
+
+// WithAnyCase ask verbalexpressions to match with or without case sensitivity
+func (v *VerbalExpression) WithAnyCase(sensitive ...bool) *VerbalExpression {
+	if len(sensitive) > 1 {
+		panic("WithAnyCase: you should give 0 or 1 argument")
+	}
+	if len(sensitive) == 1 {
+		v.anycase = sensitive[0]
+	}
+	return v
+}
+
+// SearchOneLine allow verbalexpressions to match multiline
+func (v *VerbalExpression) SearchOneLine(oneline ...bool) *VerbalExpression {
+	if len(oneline) > 1 {
+		panic("SearchOneLine: you should give 0 or 1 argument")
+	}
+	if len(oneline) == 1 {
+		v.oneline = oneline[0]
+	}
+	return v
+}
+
+// Regex return the regular expression to use to test you string.
+func (v *VerbalExpression) Regex() *regexp.Regexp {
+	modifier := ""
+	if v.anycase {
+		modifier = "i"
+	}
+
+	if v.oneline {
+		modifier = "m"
+	}
+
+	if len(modifier) > 0 {
+		v.expression = "(?" + modifier + ")" + v.expression
+	}
+
+	return regexp.MustCompile(v.expression)
+}
+
+/* proxy to regexp.Regexp functions */
+
+// Test return true if you verbalexpressions matches something in "s"
+func (v *VerbalExpression) Test(s string) bool {
+	return v.Regex().Match([]byte(s))
+}
---
+++ verbalexpressions_test.go
@@ -0,0 +1,91 @@
+package verbalexpressions
+
+import "testing"
+import "strings"
+
+func TestChaining(t *testing.T) {
+
+	exp := "http://www.google.com"
+	v := New().StartOfLine().
+		Then("http").
+		Maybe("s").
+		Then("://").
+		Maybe("www.").
+		Word().
+		Then(".").
+		Word().
+		Maybe("/").
+		EndOfLine()
+	if !v.Test(exp) {
+		t.Errorf("%v regexp doesn't match %s", v.Regex(), exp)
+	}
+}
+
+func TestRange(t *testing.T) {
+
+	exp := "abcdef 123"
+
+	v := New().Range("a", "z", 0, 9, " ")
+	if v.Regex().String() != "[a-z0-9 ]"{
+		t.Errorf("%s is not [a-z0-9 ]", v.Regex())
+	}
+	if !v.Test(exp) {
+		t.Errorf("%v regexp doesn't match %s", v.Regex(), exp)
+	}
+	exp = "ABCDEF"
+	if v.Test(exp) {
+		t.Errorf("%v regexp should not match %s", v.Regex(), exp)
+	}
+
+}
+
+func TestOneLine(t *testing.T) {
+	s := "atlanta\narkansas\nalabama\narachnophobia"
+
+	v := New().SearchOneLine(false).Find("a").EndOfLine().Regex()
+	res := v.FindAllStringIndex(s, -1)
+	if len(res) != 1 {
+		t.Errorf("%v should be length 1, %d instead", res, len(res))
+	}
+	if len(res[0]) != 2 {
+		t.Errorf("%v should be length 2, %d instead", res[0], len(res[0]))
+	}
+
+	v = New().SearchOneLine(true).Find("a").EndOfLine().Regex()
+	res = v.FindAllStringIndex(s, -1)
+	if len(res) != 3 {
+		t.Errorf("%v should be length 1, %d instead", res, len(res))
+	}
+	for _, r:= range res {
+		if len(r) != 2 {
+			t.Errorf("%v should be length 2, %d instead", r, len(r))
+		}
+	}
+}
+
+func TestExcepting(t *testing.T) {
+
+		s := "This is a simple test"
+		v := New().AnythingBut("im").Regex().FindAllString(s, -1)
+		for _, st := range v {
+			if strings.Contains(st, "i") {
+				t.Errorf("%s should not find \"i\"", st)
+			}
+			if strings.Contains(st, "m") {
+				t.Errorf("%s should not find \"m\"", st)
+			}
+		}
+}
+
+
+func TestAny (t *testing.T) {
+
+		s := "foo1 foo5 foobar"
+		v := New().Find("foo").Any("1234567890")
+		res := v.Regex().FindAllString(s, -1)
+		if len(res) != 2 {
+			t.Errorf("len(res) : %d isn't 2", len(res))
+		}
+
+}
+
