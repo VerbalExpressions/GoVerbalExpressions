809ab3bd898f9712511b5b5a11cda6ef399fe9f6
metal3d <metal3d@gmail.com>
2013-08-07 10:35:22 +0200

    ... mistake in haeader doc "Ue" => "Use"

--- verbalexpressions.go
+++ verbalexpressions.go
@@ -1,5 +1,5 @@
 // Copyright 2013 Patrice FERLET
-// Ue of this source code is governed by MIT-style
+// Use of this source code is governed by MIT-style
 // license that can be found in the LICENSE file
 package verbalexpressions
 
